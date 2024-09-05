83be3d595b4962636f5f95d94f2ec4ef7dd5ecec
sanketares <sanketares@gmail.com>
2024-09-05 00:14:31 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,20 +3,12 @@ provider "aws" {
 }
 
 
-# Create a VPC
-resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.0.0.0/16"
-
-  tags = {
-    Name = "my-vpc"
-  }
-}
 
 
 
 # Create an S3 Bucket
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "bucket-name-sanket"  # Ensure this bucket name is unique across AWS
+  bucket = "bucket-name-sanketdddd"  # Ensure this bucket name is unique across AWS
 
   tags = {
     Name = "my-bucket"
