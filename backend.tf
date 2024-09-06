cadfd0be303aff46fa5a44052b5b55a6e4a24168
sanketares <sanketares@gmail.com>
2024-09-05 20:39:12 -0700

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,10 +1,1 @@
 
-terraform {
-  backend "s3" {
-    bucket         = "sanket-s3-demo-xyzfff" # change this
-    key            = "terraform.tfstate"
-    region         = "us-west-2"
-    encrypt        = true
-    dynamodb_table = "terraform-lock1"
-  }
-}
