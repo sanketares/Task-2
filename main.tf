9978491107c2256391f2e6bb2787db82c179c92d
sanketares <sanketares@gmail.com>
2024-09-05 20:29:49 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,9 +5,16 @@ provider "aws" {
 
 
 
-
-resource "aws_instance" "santetdddsdsd" {
-  instance_type = "t2.micro"
-  ami           = "ami-053b0d53c279acc90"
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = "sanket-s3-demo-xyzfff"
 }
 
+resource "aws_dynamodb_table" "terraform_lock12" {
+  name         = "terraform-lock1"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
