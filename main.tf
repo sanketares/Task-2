34991c75286cd276c20f4a83f2a2e45b2ae13141
sanketares <sanketares@gmail.com>
2024-09-05 20:32:25 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,16 +5,3 @@ provider "aws" {
 
 
 
-resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "sanket-s3-demo-xyzfff"
-}
-
-resource "aws_dynamodb_table" "terraform_lock12" {
-  name         = "terraform-lock1"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
