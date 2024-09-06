7fc7ebd21da654113a5e7c8de424b651eab833f7
sanketares <sanketares@gmail.com>
2024-09-05 20:26:54 -0700

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,1 @@
-terraform {
-  backend "s3" {
-    bucket         = "bucket-name-sanket"
-    key            = "terraform/state"
-    region         = "us-west-2"  # Match this with the AWS region
-  }
-}
+
