4513a1eb48ad7d309bdaf57def3a422a1ff9e93c
sanketares <sanketares@gmail.com>
2024-09-05 22:24:48 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+resource "aws_instance" "sanket_in1111s" {
+  instance_type = "t2.micro"
+  ami           = "ami-05134c8ef96964280"
+  tags = {
+    Name = "sanket54321"
+  }
+}
